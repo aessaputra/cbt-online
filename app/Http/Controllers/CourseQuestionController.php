f34d1c32d5a9f33775ea9e8f199e57f3f1e809a1
aessaputra <aessaputra@yahoo.com>
2025-04-26 11:41:27 +0000

    Add question management functionality to CourseController and views

--- app/Http/Controllers/CourseQuestionController.php
+++ app/Http/Controllers/CourseQuestionController.php
@@ -2,64 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\CourseQuestion;
 use Illuminate\Http\Request;
 
 class CourseQuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create(Course $course)
+  {
+    $students = $course->students()->orderBy('id', 'DESC')->get();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return view('admin.questions.create', [
+      'course' => $course,
+      'students' => $students
+    ]);
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(CourseQuestion $courseQuestion)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CourseQuestion $courseQuestion)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(CourseQuestion $courseQuestion)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, CourseQuestion $courseQuestion)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(CourseQuestion $courseQuestion)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(CourseQuestion $courseQuestion)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, CourseQuestion $courseQuestion)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(CourseQuestion $courseQuestion)
+  {
+    //
+  }
 }
