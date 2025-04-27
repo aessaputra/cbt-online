c69a8f1c16296160ba7513494f1f01fe9e4229f1
aessaputra <aessaputra@yahoo.com>
2025-04-27 16:37:31 +0000

    Implement student management view and add functionality to create new students in CourseStudentController

--- app/Http/Controllers/CourseStudentController.php
+++ app/Http/Controllers/CourseStudentController.php
@@ -2,64 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\CourseStudent;
 use Illuminate\Http\Request;
 
 class CourseStudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create(Course $course)
+  {
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    $students = $course->students()->orderBy('id', 'DESC')->get();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(CourseStudent $courseStudent)
-    {
-        //
-    }
+    return view('admin.students.add_student', [
+      'course' => $course,
+      'students' => $students
+    ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CourseStudent $courseStudent)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, CourseStudent $courseStudent)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(CourseStudent $courseStudent)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(CourseStudent $courseStudent)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(CourseStudent $courseStudent)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, CourseStudent $courseStudent)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(CourseStudent $courseStudent)
+  {
+    //
+  }
 }
