4cce5e8add884c8e6a8ef3b5f9d0c80b208eee46
aessaputra <aessaputra@yahoo.com>
2025-04-27 18:03:00 +0000

    Enhance LearningController to calculate next unanswered question for each course; update view to link to course with next question

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CourseQuestion;
+use App\Models\StudentAnswer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +14,26 @@ class LearningController extends Controller
     $user = Auth::user();
 
     $my_courses = $user->courses()->with('category')->orderBy('id', 'DESC')->get();
+
+    foreach ($my_courses as $course) {
+      $totalQuestionsCount = $course->questions()->count();
+
+      $answeredQuestionsCount = StudentAnswer::where('user_id', $user->id)->whereHas('question', function ($query) use ($course) {
+        $query->where('course_id', $course->id);
+      })->distinc()->count('question_id');
+
+      if ($answeredQuestionsCount < $totalQuestionsCount) {
+        $firstUnansweredQuestion = CourseQuestion::where('course_id', $course->id)
+          ->whereNotIn('id', function ($query) use ($user) {
+            $query->select('question_id')->from('student_answers')->where('user_id', $user->id);
+          })->orderBy('id', 'ASC')->first();
+
+        $course->nextQuestionId = $firstUnansweredQuestion ? $firstUnansweredQuestion->id : null;
+      }
+      else {
+        $course->nextQuestionId = null;
+      }
+    }
 
     return view('student.courses.index', [
       'my_courses' => $my_courses,
