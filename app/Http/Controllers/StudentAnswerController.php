4f70fabc6cf0b61cba8134a53de15bc94afdd14c
aessaputra <aessaputra@yahoo.com>
2025-04-28 04:16:14 +0000

    Enhance StudentAnswerController to handle answer submission with validation and error handling; update learning view to correctly associate answer labels with their IDs

--- app/Http/Controllers/StudentAnswerController.php
+++ app/Http/Controllers/StudentAnswerController.php
@@ -2,64 +2,120 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\CourseAnswer;
+use App\Models\CourseQuestion;
 use App\Models\StudentAnswer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class StudentAnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request, Course $course, $question)
+  {
+    $question_detials = CourseQuestion::where('id', $question)->first();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(StudentAnswer $studentAnswer)
-    {
-        //
-    }
+    $validated = $request->validate([
+      'answer_id' => 'required|exists:course_answers,id',
+    ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(StudentAnswer $studentAnswer)
-    {
-        //
-    }
+    DB::beginTransaction();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, StudentAnswer $studentAnswer)
-    {
-        //
-    }
+    try {
+      $selectedAnswer = CourseAnswer::find($validated['answer_id']);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(StudentAnswer $studentAnswer)
-    {
-        //
+      dd($selectedAnswer);
+
+      if ($selectedAnswer->course_question_id) {
+        $error = ValidationException::withMessages([
+          'system_error' => ['System error!' . ['Jawaban tidak tersedia pada pertanyaan!']]
+        ]);
+
+        throw $error;
+      }
+
+      $existingAnswer = StudentAnswer::where('user_id', Auth::id())->where('course_question_id', $question)->first();
+
+      if ($existingAnswer) {
+        $error = ValidationException::withMessages([
+          'system_error' => ['System error!' . ['Kamu telah menjawab pertanyaan ini sebelumbya!']]
+        ]);
+
+        throw $error;
+      }
+
+      $answerValue = $selectedAnswer->is_correct ? 'correct' : 'wrong';
+
+      StudentAnswer::create([
+        'user_id' => Auth::id(),
+        'course_question_id' => $question,
+        'answer' => $answerValue
+      ]);
+
+      DB::commit();
+
+      
+
+    } catch (\Exception $e) {
+      DB::rollBack();
+      $error = ValidationException::withMessages([
+        'system_error' => ['System error!' . $e->getMessage()]
+      ]);
+
+      throw $error;
     }
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(StudentAnswer $studentAnswer)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(StudentAnswer $studentAnswer)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, StudentAnswer $studentAnswer)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(StudentAnswer $studentAnswer)
+  {
+    //
+  }
 }
