fff727f9b89d57f1260a453d30b11a48bfd23358
aessaputra <aessaputra@yahoo.com>
2025-04-28 14:24:35 +0000

    feat: Enhance CourseStudentController to display student statuses and create index view for managing students

--- app/Http/Controllers/CourseStudentController.php
+++ app/Http/Controllers/CourseStudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\CourseStudent;
+use App\Models\StudentAnswer;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -14,9 +15,34 @@ class CourseStudentController extends Controller
   /**
    * Display a listing of the resource.
    */
-  public function index()
+  public function index(Course $course)
   {
-    //
+    $students = $course->students()->orderBy('id', 'DESC')->get();
+    $questions = $course->questions()->orderBy('id', 'DESC')->get();
+    $totalQuestions = $questions->count();
+
+    foreach ($students as $student) {
+      $studentAnswers = StudentAnswer::whereHas('question', function ($query) use ($course) {
+        $query->where('course_id', $course->id);
+      })->where('user_id', $student->id)->get();
+
+      $answerCount = $studentAnswers->count();
+      $correctAnswersCount = $studentAnswers->where('answer', 'correct')->count();
+
+      if ($answerCount == 0) {
+        $student->status = 'Not Started';
+      } elseif ($correctAnswersCount < $totalQuestions) {
+        $student->status = 'Not Passed';
+      } elseif ($correctAnswersCount == $totalQuestions) {
+        $student->status = 'Passed';
+      }
+    }
+
+    return view('admin.students.index', [
+      'course' => $course,
+      'questions' => $questions,
+      'students' => $students,
+    ]);
   }
 
   /**
